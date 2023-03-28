16c7ec7386e4f4343d4d008716fcd3049d59cc0e
rehmanessanidev <72720538+rehmanessanidev@users.noreply.github.com>
2023-03-28 15:03:51 -0500

    Update patienthomepage.php

--- patienthomepage.php
+++ patienthomepage.php
@@ -1,94 +1,18 @@
-<?php 
-	ob_start();
-	session_start();
-//ob_start();
-//ob_start();
+<?php
+		//session_start();
+		
+		session_start();
+		ob_start();
 
-	include("dbh-inc.php");
-	include("functions.php");
 
-	$user_data = check_login($conn);
-?>
+		include("dbh-inc.php");
+		include("functions.php");
 
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Medical Clinic Home Page</title>
-	<link rel="stylesheet" type="text/css" href="styles.css">
-</head>
-<style>
-	table {
-		border-collapse: collapse;
-		width: 100%;
-	}
+		$user_data = check_login($conn);
+		//include("dbh-inc.php");
+		//include("functions.php");
 
-	th, td {
-		text-align: center;
-		padding: 8px;
-		border: 1px solid #ddd;
-	}
-
-	tr:nth-child(even) {
-		background-color: #f2f2f2;
-	}
-
-	h1 {
-		font-size: 50px;
-		margin-bottom: 20px;
-	}
-
-	.container {
-		margin: auto;
-		max-width: 800px;
-		padding: 20px;
-	}
-</style>
-<body>
-	<header>
-		<h1><center>Discount Clinic</center></h1>
-		<nav>
-			<ul>
-				<li class ="active"><a href="#">Home</a></li>
-			<!-- <li><a href="patient_appointment_list.php">Appointments</a></li> -->
-				<li><a href="patient_profile.php">Profile</a></li>
-				<li><a href="patientappointments.php">Schedule Appointment</a></li>
-                <li><a href="transactions.php">Transactions</a></li>
-				<li><a href = "testing.php">Patient Form</a></li>
-				<li><a href="logout.php">Logout</a></li>
-			</ul>
-		</nav>
-	</header>
-	<main>
-		<h2><center>Welcome, <?php echo $user_data['username']; ?></center></h2>
-        <h4>About Us</h4>
-		<p>Our clinic provides high-quality medical services to patients of all ages at a minimal cost. We have a team of experienced doctors who are dedicated to your health and well-being. Book an appointment at your convenience at one of our various offices across the nation. </p>
-		<div class="services">
-			<h3>Our Services</h3>
-				<p>General Medicine</p>
-				<p>Pediatrics</p>
-				<p>Cardiology</p>
-				<p>Dermatology</p>
-		</div>
-	</main>
-</html>
-<h3> UPCOMING Appointments</h3>
-<table>
-  <thead>
-    <tr>
-      <th>Appointment ID</th>
-      <th>Doctor Name</th>
-      <th>Office ID</th>
-      <th>Date</th>
-      <th>Time</th>
-      <th>Address</th>
-      <th>City</th>
-      <th>State</th>
-      <th>Zipcode</th>
-      <th>Amount</th>
-    </tr>
-  </thead>
-  <tbody>
-	<?php
+		//$user_data = check_login($conn);
 
 		$TEST = $user_data['username'];
 		$query = "SELECT user_id FROM user WHERE username = '$TEST'";
@@ -173,24 +97,89 @@
 		$conn->close();
 		
 		ob_flush();
+?>
 
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Medical Clinic Home Page</title>
+	<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<style>
+	table {
+		border-collapse: collapse;
+		width: 100%;
+	}
 
+	th, td {
+		text-align: center;
+		padding: 8px;
+		border: 1px solid #ddd;
+	}
 
+	tr:nth-child(even) {
+		background-color: #f2f2f2;
+	}
 
+	h1 {
+		font-size: 50px;
+		margin-bottom: 20px;
+	}
 
+	.container {
+		margin: auto;
+		max-width: 800px;
+		padding: 20px;
+	}
+</style>
+<body>
+	<header>
+		<h1><center>Discount Clinic</center></h1>
+		<nav>
+			<ul>
+				<li class ="active"><a href="#">Home</a></li>
+			<!-- <li><a href="patient_appointment_list.php">Appointments</a></li> -->
+				<li><a href="patient_profile.php">Profile</a></li>
+				<li><a href="patientappointments.php">Schedule Appointment</a></li>
+                <li><a href="transactions.php">Transactions</a></li>
+				<li><a href = "testing.php">Patient Form</a></li>
+				<li><a href="logout.php">Logout</a></li>
+			</ul>
+		</nav>
+	</header>
+	<main>
+		<h2><center>Welcome, <?php echo $user_data['username']; ?></center></h2>
+        <h4>About Us</h4>
+		<p>Our clinic provides high-quality medical services to patients of all ages at a minimal cost. We have a team of experienced doctors who are dedicated to your health and well-being. Book an appointment at your convenience at one of our various offices across the nation. </p>
+		<div class="services">
+			<h3>Our Services</h3>
+				<p>General Medicine</p>
+				<p>Pediatrics</p>
+				<p>Cardiology</p>
+				<p>Dermatology</p>
+		</div>
+	</main>
+</html>
+<h3> UPCOMING Appointments</h3>
+<table>
+  <thead>
+    <tr>
+      <th>Appointment ID</th>
+      <th>Doctor Name</th>
+      <th>Office ID</th>
+      <th>Date</th>
+      <th>Time</th>
+      <th>Address</th>
+      <th>City</th>
+      <th>State</th>
+      <th>Zipcode</th>
+      <th>Amount</th>
+    </tr>
+  </thead>
+  <tbody>
 
-	?>
 
   </tbody>
 </table>
 </body>
 </html>
-
-
-
-
-
-
-
-
-
